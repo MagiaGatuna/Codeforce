1cadf8b0efc79b164d937bc94fbca1148092980a
MagiCat <corinavicky07@gmail.com>
2026-04-07 19:52:10 -0400

    Add files via upload

---
+++ Practica-02/C. Rectangle in Circle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int a, b, r;
+    cin>>a>>b>>r;
+    if ((a*a)+(b*b)<=(2*r)*(2*r)) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+    return 0;
+}
